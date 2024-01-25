71df6cf25a42ca8f1ac18b09a216963beed04fd0
Abed-Alfattah Sowaity <swaiti393@gmail.com>
2024-01-25 14:17:20 +0200

    User Avatars And Profile Pictures

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,22 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show]
   def index
     @should_render_navbar = true
+  end
+
+  def appearance
+    @should_render_navbar = true
+  end
+
+  def show
+    redirect_to dashboard_path if @user.nil?
+  end
+
+  private
+
+  def set_user
+    # localhost:3000/1
+    @user = User.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { 
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+}
   get 'dashboard', to: 'dashboard#index'
+  get 'appearance', to: 'dashboard#appearance'
+
+
   root 'dashboard#index'
+
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
